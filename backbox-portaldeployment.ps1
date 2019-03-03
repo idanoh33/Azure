287ca98a6b68850ff35048166c5f03936daa54e3
Idan Ohayon <idanoh33@gmail.com>
2019-03-03 11:56:47 +0200

    Update backbox-portaldeployment.ps1

--- backbox-portaldeployment.ps1
+++ backbox-portaldeployment.ps1
@@ -13,35 +13,28 @@
         $sourceVHDURI = 'https://backboxstgeastus.blob.core.windows.net/eastus-cont/BackBoxv6tryFixed.vhd'
     )
 
-    Begin
-    {
     $resourceGroupName = "Backbox-rg"
     $storageaccountname = "backboxstg" + ( -join (1..100 |Get-Random -Count 6))
     $contname = "$storageaccountname-cont"
     $vhd = Split-Path -Leaf $sourceVHDURI 
-    }
-    Process
-    {
-        if (!(Get-AzureRmResourceGroup -name $resourceGroupName -ErrorAction SilentlyContinue )) {
-            $RG = New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
-        } else {$RG = Get-AzureRmResourceGroup -name $resourceGroupName}
+   
+    if (!(Get-AzureRmResourceGroup -name $resourceGroupName -ErrorAction SilentlyContinue )) {
+        $RG = New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+    } else {$RG = Get-AzureRmResourceGroup -name $resourceGroupName}
 
-        $stg = New-AzureRmStorageAccount -ResourceGroupName $rg.ResourceGroupName -Name $storageaccountname -SkuName Standard_LRS -Location $location -Kind StorageV2 -AccessTier Cool
-        $cont = New-AzureRmStorageContainer -StorageAccountName $storageaccountname -ResourceGroupName $rg.ResourceGroupName -Name $contname
+    $stg = New-AzureRmStorageAccount -ResourceGroupName $rg.ResourceGroupName -Name $storageaccountname -SkuName Standard_LRS -Location $location -Kind StorageV2 -AccessTier Cool
+    $cont = New-AzureRmStorageContainer -StorageAccountName $storageaccountname -ResourceGroupName $rg.ResourceGroupName -Name $contname
 
-        Write-Output "Start Time: $(get-date)"
-        Write-Output "Start copy backbox VHD"
+    Write-Output "Start Time: $(get-date)"
+    Write-Output "Start copy backbox VHD"
 
-        $blob = Start-AzureStorageBlobCopy -AbsoluteUri $sourceVHDURI  -DestContainer $cont.Name -DestBlob $vhd -DestContext $stg.Context
-        $blob| Get-AzureStorageBlobCopyState
+    $blob = Start-AzureStorageBlobCopy -AbsoluteUri $sourceVHDURI  -DestContainer $cont.Name -DestBlob $vhd -DestContext $stg.Context
+    $blob| Get-AzureStorageBlobCopyState
 
-        Do {Write-Output "copy status is: $(($blob| Get-AzureStorageBlobCopyState).Status)"; sleep -Seconds 10} Until (($blob| Get-AzureStorageBlobCopyState).Status -ne "Pending")
-        Write-Output "End Time: $(get-date)"
+    Do {Write-Output "copy status is: $(($blob| Get-AzureStorageBlobCopyState).Status)"; sleep -Seconds 10} Until (($blob| Get-AzureStorageBlobCopyState).Status -ne "Pending")
+    Write-Output "End Time: $(get-date)"
 
-    }
-    End
-    {
     $newUri = "$($blob.context.BlobEndPoint)" + "$($cont.name)/" + "$vhd" #+ $sas
     Write-Output "New URI: $newUri"  
 
-    }
+
