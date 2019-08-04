6081c0d0e2003b293d7dd6587509f16bd7cab6dc
Idan Ohayon <idanoh33@gmail.com>
2019-08-04 16:01:37 +0300

    Update Join-domain.ps1

--- Join-domain.ps1
+++ Join-domain.ps1
@@ -2,7 +2,7 @@ param([String]$password)
 $domainname= "lab.local"
 $username= "idadmin"
 $user= "$domainname\$username"
-$password= $password | ConvertTo-SecureString -AsPlainText -Force
+$password= $args[0] | ConvertTo-SecureString -AsPlainText -Force
 
 $cred = new-object -typename System.Management.Automation.PSCredential `
          -argumentlist $username, $password
