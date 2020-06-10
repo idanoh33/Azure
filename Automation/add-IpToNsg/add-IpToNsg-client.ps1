8e763e60ac01621d56856f8389c7e27e532d7d89
Idan Ohayon <idanoh33@gmail.com>
2020-06-10 21:00:54 +0300

    add temp

---
+++ Automation/add-IpToNsg/add-IpToNsg-client.ps1
@@ -0,0 +1,50 @@
+﻿<#
+.Synopsis
+   This script will run the client computer, resolve the public IP and trigger a webhook with the IP parameter
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   This script will run the client computer, resolve the public IP and trigger a webhook with the IP parameter
+   The webhook will trigger Azure automation runbook which add the client IP to NSG white list
+   
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Add-IpToNsg
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+       [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+                   $WebhookUri = "Webhook URI goes here"
+    )
+
+    Begin
+    {
+        
+        $ip= (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
+ 
+        $IpAddress= @(
+                    @{ IpAddress="$ip"}
+                     )
+ 
+        $body = ConvertTo-Json -InputObject $IpAddress
+        $header = @{ message = "$env:USERNAME" }
+    }
+    Process
+    {
+        $response = Invoke-WebRequest -Method Post -Uri $uri -Body $body -Headers $header
+        $jobid = (ConvertFrom-Json ($response.Content)).jobids[0]
+    }
+    End
+    {
+        $jobid 
+    }
+}
+
+
+Add-IpToNsg -WebhookUri ''
---
+++ Automation/add-IpToNsg/add-IpToNsg-runbook.ps1
@@ -0,0 +1,60 @@
+﻿    [CmdletBinding()]
+    param
+    (
+        [Parameter (Mandatory = $false)]
+        [object] $WebhookData
+    )
+
+    $IpAddress = (ConvertFrom-Json -InputObject $WebhookData.RequestBody).IpAddress
+    write-output $IpAddress
+    if ($IpAddress -eq $null) {break}
+
+    $connectionName = "AzureRunAsConnection"
+    
+    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
+
+    $connectionResult = Connect-AzAccount `
+   -ServicePrincipal `
+   -TenantId $servicePrincipalConnection.TenantId `
+   -ApplicationId $servicePrincipalConnection.ApplicationId `
+   -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+
+   $subscriptions= Get-AzSubscription | select-azSubscription
+
+   $NsgName= "vce-vm-nsg"
+   $RgName= "vce-rg"
+   $location= "westeurope"
+
+    $nsg = Get-AzNetworkSecurityGroup -Name $NsgName -ResourceGroupName $RgName
+    if (!$nsg){
+        $nsgRuleRDP = New-AzNetworkSecurityRuleConfig `
+          -Name "RDP"  `
+          -Protocol "Tcp" `
+          -Direction "Inbound" `
+          -Priority 1000 `
+          -SourceAddressPrefix $ip `
+          -SourcePortRange * `
+          -DestinationAddressPrefix * `
+          -DestinationPortRange 3389 `
+          -Access "Allow"
+
+        $nsg = New-AzNetworkSecurityGroup `
+          -ResourceGroupName $resourcegroupname `
+          -Location $location `
+          -Name "$prefix-nsg" `
+          -SecurityRules $nsgRuleSSH 
+    }
+    
+    $rule= $nsg | Get-AzNetworkSecurityRuleConfig -Name "RDP" 
+    if (!$rule){}
+    $ips = $rule.SourceAddressPrefix
+    $ips
+    $ips += $IpAddress
+
+    #$nsg = Get-AzNetworkSecurityGroup -Name vce-vm-nsg -ResourceGroupName vce-rg
+    Set-AzNetworkSecurityRuleConfig -SourceAddressPrefix $ips -NetworkSecurityGroup $nsg -Name RDP `
+        -Protocol Tcp -SourcePortRange * `
+        -DestinationPortRange 3389 -DestinationAddressPrefix * `
+        -Access allow -Priority 300 -Direction Inbound
+
+    $null = Set-AzNetworkSecurityGroup -NetworkSecurityGroup $nsg
